b31142a58b9a01f415a007c280cde225703e6881
Ftps <francisco.tomas@tecnico.ulisboa.pt>
2018-12-15 18:41:57 +0000

    red nozzle builder init

---
+++ RED - Engine/Nozzle_Sim.m
@@ -0,0 +1,9 @@
+pe = 101325;    %sea level pressure in Pa
+Tamb = 293.15;  %average temperature in K (20ºC)
+
+
+de = 0.06; %change values for performance
+dc = 0.01; %change values for performance
+
+Ae = pi*(de^2)/4;
+Ac = pi*(dc^2)/4;
